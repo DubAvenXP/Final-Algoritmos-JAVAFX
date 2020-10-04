cedd382db77571780683325fa967c003b5114bfa
Alejandro Dubon <alejandrodubon88@gmail.com>
2020-10-04 09:49:22 -0600

    solucionando algunas cosas a primera hora

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@ F
 src/sample/sample.fxml,2\f\2f769fc3b0dd177c3365dff558843905f8609d7b
 J
 src/sample/Controller.java,a\9\a952ee24ccaf329067caf45d8e61416ce808b0ea
+R
+".idea/libraries/jfoenix_9_0_10.xml,8\f\8fed050a061d95fed81ab25e443bf0d42754c1b5
+V
+&.idea/libraries/postgresql_42_2_16.xml,b\9\b9096fc93f7c928475d92114b9ee860cda37722c
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,20 +5,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("Formulario");
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        Connect.connection();
+        launch(args);
+        //Connect.connection();
     }
 }
